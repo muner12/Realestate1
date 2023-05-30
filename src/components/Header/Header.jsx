c8900671ff90dc6d06f9ac67e3d7df16a5e9aa02
Munir <bakhtmuner06@gmail.com>
2023-05-30 17:41:32 +0500

    First Commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <section className='h-wrapper'>
+          <div className="h-container">
+            <img src="./logo.png" width={100}alt=""/>
+          </div>
+        </section>
+    );
+}
+
+export default Header;
